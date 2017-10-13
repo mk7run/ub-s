fa0ab8a2e7615522df96cb270f04497d4482e128
Melissa Kanhirun <mk7run@gmail.com>
2017-10-13 10:04:48 -0500

    Update users.rb

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -7,8 +7,9 @@ end
 # retrieve info from registration to database
 post '/users' do
   @user = User.new(params[:user])
-
+  
   if @user.save
+    session[:user_id] = @user.id
     redirect '/'
   else
     @errors = @user.errors.full_messages
